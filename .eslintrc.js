2fd086410a47c0be881454f6eee3b7fda65aff42
donghyeon413 <ghhrn413@naver.com>
2023-01-10 16:07:00 +0900

    dom및 유틸함수 라이브러리 만들기

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
     },
     "rules": {
         "no-unused-vars":'off',
-        "no-undef":'warn'
+        "no-undef":'off'
     }
 }
--- client/chapter/dom/02.nodeClass.js
+++ client/chapter/dom/02.nodeClass.js
@@ -24,18 +24,40 @@
 
 /* 노드 정보 ------------------------------------------------------------- */
 
+
+let first = getNode('.first');
+
+
+
 // - nodeType
+console.log( first.nodeType === document.ELEMENT_NODE );
+console.log( first.nodeType === 1 );
+
+
+
 // - nodeName (vs. tagName)
+console.log( first.nodeName === 'SPAN' );
+console.log( first.tagName !== 'SPAN' );
 
 
 /* 노드 콘텐츠 읽기/쓰기 ---------------------------------------------------- */
 
 // - innerHTML
+first.innerHTML = 'like lion';
+
+
 // * 기존 내용 삭제
+first.innerHTML = '';
+
 // * 기존 내용과 새로운 내용을 합친 새로운 내용을 씀
+first.innerHTML += '<strong>like lion</strong>';
 
 // - textContent
+console.log( first.textContent ); // get 
+
 // * 요소 내의 텍스트에 접근
+first.textContent = '30% 파격 세일' // set
+
 // * 태그는 제외하고 오로지 텍스트만 추출
 
 
@@ -44,3 +66,44 @@
 // - hidden
 // * hidden은 HTML 속성으로, DOM 프로퍼티로 사용 가능
 // * hidden 프로퍼티는 기술적으로 style="display:none"와 동일
+
+
+getNode('h1').hidden = true;
+
+let toggle = false;
+setInterval(() => {
+  getNode('h1').hidden = toggle ? false : true
+  toggle = !toggle
+}, 100);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
