b967bb9da4585b492fee1ac5c5b908ac763e7190
donghyeon413 <ghhrn413@naver.com>
2023-01-16 13:34:18 +0900

    visible / invisible 추가 및 핸들러 이름 수정

--- client/lib/dom/controlElement.js
+++ client/lib/dom/controlElement.js
@@ -3,14 +3,13 @@ import { isElement } from "../utils/typeOf.js";
 
 
 
-//JSDoc
 
 /**
- * 
  * @function isElement checkElement
  * @param {HTMLElement} node
- * @return set disabled
-*/
+ * @returns set disabled 
+ */
+
 export function disableElement(node){
 
   if(!isElement(node)){
@@ -18,6 +17,7 @@ export function disableElement(node){
   }
   node.disabled = true;
 }
+
 
 
 export function enableElement(node){
@@ -28,7 +28,21 @@ export function enableElement(node){
   node.disabled = false;
 }
 
-disableElement()
+
+export function visibleElement(node){ // 보여야함
+  if(!isElement(node)){
+    typeError('visibleElement 함수의 인자는 DOM 요소 노드 이어야 합니다.')
+  }
+  node.hidden = false;
+}
+
+
+export function invisibleElement(node){ // 안보여야함 
+  if(!isElement(node)){
+    typeError('invisibleElement 함수의 인자는 DOM 요소 노드 이어야 합니다.')
+  }
+  node.hidden = true;
+}
 
 
 
