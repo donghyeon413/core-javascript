757af7dcbb2521c09ff28c11c397a546c1d36255
donghyeon413 <ghhrn413@naver.com>
2023-01-19 14:11:04 +0900

    .

--- client/main.js
+++ client/main.js
@@ -1,47 +1,96 @@
+/* global gsap */
 
 
-import { 
-  insertLast,
+
+import {
+  attr,
   tiger,
   delayP,
-  createUserCard,
-  getNode
- } from "./lib/index.js";
+  insertLast,
+  changeColor,
+  getNode as $,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard,
+} from './lib/index.js';
 
-
-
-// rendingUserList 함수 만들기 
+// rendingUserList 함수 만들기
 // ajax (tiger) get user List
 
-
 // 유저 카드 생성
-// 생성된 카드로 랜더링 
+// 생성된 카드로 랜더링
 
 //  1. userList.js로 갑니다.
 //  2. renderUserCard 함수를 만들기
 //  3. 만들어진 함수 안에 createUserCard를 던지고,
-//  4. renderUserCard함수를 사용했을 떄 렌더링이 잘 될 수 있도록.
+//  4. renderUserCard함수를 사용했을 때  랜더링이 잘 될 수 있도록.
 
-const userCardContainer = getNode('.user-card-inner');
 
-async function rendingUserList(){
+
+const userCardContainer = $('.user-card-inner');
+
+async function rendingUserList() {
+
+  renderSpinner(userCardContainer)
+
+  try{
+    await delayP(2000)
+
+    $('.loadingSpinner').remove();
+
+    let response = await tiger.get( 'https://jsonplaceholder.typicode.com/users' );
   
-  let response = await tiger.get('https://jsonplaceholder.typicode.com/users/1')
+    let userData = response.data;
+    // userData.forEach(data=> renderUserCard(userCardContainer,data))
+    userData.forEach((data)=> {
+      renderUserCard(userCardContainer,data)
+    })
   
-  let userData = response.data;
+    changeColor('.user-card');
+    
+    gsap.to(gsap.utils.toArray('.user-card'),{
+      x:0,
+      opacity:1,
+      duration:1.5,
+      stagger: 0.2,
+    })
+  }catch(err){
+    // console.log(err);
+    renderEmptyCard(userCardContainer)
 
-  console.log(userData);
+  }
+}
 
-  renderUserCard()
-
-  insertLast('userCardContainer',createUserCard(userData))
+rendingUserList();
 
 
+
+// 삭제 버튼을 클릭하면 콘솔창에 '삭제' 글자가 출력이 될 수 있도록 만들어 주세요.
+
+
+
+function handler(e){
+  let deleteButton = e.target.closest('button');
+  let article = e.target.closest('article');
+
+  if(!deleteButton || !article) return;
+
+   
+  let id = attr(article,'data-index').slice(5);
+
+  console.log(id);
+  
 }
 
 
+userCardContainer.addEventListener('click',handler)
 
-rendingUserList();
+
+
+
+
+
+
 
 
 
